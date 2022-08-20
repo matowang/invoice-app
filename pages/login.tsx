7b71c08443adb27f94ad2015070ac0e0bb339c1e
Matthew Wang <wmatthew123@gmail.com>
2022-08-20 20:24:39 +0800

    signup login links

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,13 @@
+import Link from "next/link";
 import LoginFormContainer from "../src/user/LoginFormContainer";
 import NotAuthGuard from "../src/user/NotAuthGuard";
-
-import { useRouter } from 'next/router';
-import { useAuth } from '../src/user/AuthContext';
-import { useEffect } from "react";
-
 const Login = () => {
     return <NotAuthGuard>
-        <div className='my-40 mx-20 flex justify-center relative'>
+        <div className='my-40 mx-20 flex flex-col items-center relative'>
             <div className='max-w-md w-full'>
                 <LoginFormContainer />
             </div>
+            <Link href='/register'><a className="my-5">Don't have an account? Sign-up here.</a></Link>
         </div>
     </NotAuthGuard>
 }
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import RegisterFormContainer from "../src/user/RegisterFormContainer";
 
@@ -5,10 +6,11 @@ const Register = () => {
 
     const router = useRouter();
 
-    return <div className='my-40 mx-20 flex justify-center relative'>
+    return <div className='my-40 mx-20 flex flex-col items-center relative'>
         <div className='max-w-md w-full'>
             <RegisterFormContainer onRegisterSuccess={() => { router.push('/login') }} />
         </div>
+        <Link href='/login'><a className="my-5">Already have an account? Login here.</a></Link>
     </div>
 }
 
